323fe4403ae0f227e0c7f807f12e23df2e3c720f
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-04-07 02:17:00 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const audioPlayer = document.getElementById("audioPlayer");
+  
+    audioPlayer.addEventListener("play", function() {
+      alert("OST The Reasons of My Smiles - sedang diputar!");
+    });
+  });
+  
